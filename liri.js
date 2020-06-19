d52923a296c70ccc1363699244383a785001955d
Nicole Apsche <napsche@kgtiger.com>
2020-06-19 09:15:47 -0400

    changing dot to ., adding axios

--- liri.js
+++ liri.js
@@ -1,8 +1,26 @@
 var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
-var inquirer = require("inquirer");
+// var inquirer = require("inquirer");
+var SPOTIFY_ID = "a8f4bd03189249a99e313ee104d916a7";
+var SPOTIFY_SECRET = "f37cf829f64a4a76ae28fc93ffb56e61";
+var request = require('request'); // "Request" library
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
 
-var spotify = new spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
+
+spotify
+  .search({ type: 'track', query: 'All the Small Things' })
+  .then(function(response) {
+    response.tracks.items.forEach(function(item) {
+        console.log(JSON.stringify(item));
+    });
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
 
 var Song = function(artist, title, preview, album) {
     this.artist = artist;
